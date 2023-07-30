e2d4968ec2b410cd11e1b28ac11160b7b426c02c
Mustafa Kan <79532179+mustafakan01@users.noreply.github.com>
2023-07-30 19:44:29 +0300

    commit

--- WebApi/Movie.cs
+++ WebApi/Movie.cs
@@ -4,7 +4,19 @@ namespace WebApi
 {
     public class Movie
     {
-        
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int GenreId { get; set; } 
+
+        public string Director { get; set; }
+
+        public string Artist { get; set; }
+
+        public decimal Price { get; set; }
     } 
     
 }
